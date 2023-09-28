4847b57810db747ccd7635ce4c02fc38658b9044
hugomers <hugomo0897@gmail.com>
2023-09-27 20:02:22 -0600

    se pone la configuracion de diferentes rutas para hacer el cuestionario

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const http  = require("http");
 const app = express();
 const server = http.createServer(app);
 
-app.get("/", function(req,res){
-    res.redirect("https://wkf.ms/3YdiWxU");
+// app.get("/", function(req,res){
+//     res.redirect("https://wkf.ms/3YdiWxU");// evaluacion de mochila
+// })
+
+
+app.get("/juguete", function(req,res){
+    res.redirect("https://wkf.ms/3PD6LXk");// evaluacion de mochila
+})
+
+app.get("/calculadora", function(req,res){
+    res.redirect("https://forms.monday.com/forms/4f441a1259f1cb4ff3a4ab517c5f40dc?r=use1");// evaluacion de mochila
+})
+
+app.get("/luces", function(req,res){
+    res.redirect("https://forms.monday.com/forms/5910248434744eb5778893b4b369f41b?r=use1");// evaluacion de mochila
 })
 
 
